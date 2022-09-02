94b0c4493c1f15e164e41f5d22288b1b550b560a
Anton Darovsky <antondarovski97@gmail.com>
2022-09-02 15:18:36 +0300

    master

---
+++ YandexSeleniumTests/YandexSeleniumTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YandexSeleniumTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
