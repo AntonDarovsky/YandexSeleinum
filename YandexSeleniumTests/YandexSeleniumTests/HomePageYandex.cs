18e12e6008bd7cd32c5b27cc3db472bdef11a267
Anton Darovsky <antondarovski97@gmail.com>
2022-09-04 17:56:11 +0300

    YandexTests

---
+++ YandexSeleniumTests/YandexSeleniumTests/HomePageYandex.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YandexSeleniumTests
+{
+    public class HomePageYandex : BasePage
+    {
+        public static readonly By _LoginButton1 = By.CssSelector(".user__login-link");
+        public static readonly By _LoginButton2 = By.ClassName("Button2");
+        public HomePageYandex(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void ClickLoginButton()
+        {
+            Driver.FindElement(_LoginButton1).Click();
+
+        }
+
+        public LoginPageYandex GoToLoginPage()
+        {
+            Driver.FindElement(_LoginButton2).Click();
+            return new LoginPageYandex(Driver);
+        }
+    }
+}
